a2d434e9ee89c6467749522f9813cefc0523c486
Nata <JosephAlvarengaBeech@protonmail.com>
2019-04-20 11:50:40 -0700

    created game logic

--- memory _game/js/main.js
+++ memory _game/js/main.js
@@ -1,7 +1,32 @@
-var CardOne = "Queen";
-var CardTwo = "King";
-var CardThree = ""     ;
-var CardFour = " ";
+//array of cards
+//temporary array of pissible possible cards
 
-console.log("User Flipped" + CardOne)
-console.log("User Flipped" + CardTwo)
+//
+
+
+var cards = ["queen","queen","king","king"]
+
+var cardsInPlay = []
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+
+cardsInPlay.push (cardOne , cardTwo );
+
+console.log ("user flipped Queen.");
+console.log ("User flipped King.");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ) {
+ 
+
+alert("You Found a match!");
+}
+else  { 
+    (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1] )
+alert("sorry,try again ! ")
+}
+
+
+
+
