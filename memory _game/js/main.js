2fd9c0ef2b69becfdcb2bd0f5a6aaabacde90942
Nata <JosephAlvarengaBeech@protonmail.com>
2019-04-22 14:58:21 -0700

    created game logic

--- memory _game/js/main.js
+++ memory _game/js/main.js
@@ -4,13 +4,34 @@
 //
 
 
-var cards = ["queen","queen","king","king"]
+var cards = [
+    {
+       rank:"queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank:"queen",
+        suit:"diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+     },
+     {
+        rank:"king",
+        suit:"hearts",
+        cardImage:"images/king-of-hearts.png"
+     },
+     {
+        rank:"king",
+        suit:"diamonds",
+        cardImage: "images/king-of-diamonds.png"
+     }
+];
 
 var cardsInPlay = []
 
 function checkForMatch (){
 
-    if (cardsInPlay[0] === cardsInPlay[1]) {
+    if ( cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
       } else {
         console.log("Sorry, try again.");
@@ -19,8 +40,10 @@ function checkForMatch (){
 
 function flipCard (cardId){
 
-    console.log("User flipped" + cards[cardId])
-    cardsInPlay.push (cards[cardId]);
+    console.log("User flipped" + cards[cardId].rank)
+    cardsInPlay.push (cards[cardId].rank);
+    console.log (cards[cardId].suit);
+    console.log (cards[cardId].cardImage);
  
 checkForMatch ();
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ) {
@@ -33,4 +56,4 @@ alert("sorry,try again ! ");
 }
 };
 flipCard (0);
-flipCard (2);
+flipCard (1);
