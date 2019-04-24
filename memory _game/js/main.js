ea670ea1cad5a00d9a8609d77c4228465ac7f7da
Nata <JosephAlvarengaBeech@protonmail.com>
2019-04-23 19:10:40 -0700

    created game logic

--- memory _game/js/main.js
+++ memory _game/js/main.js
@@ -2,6 +2,7 @@
 //temporary array of pissible possible cards
 
 //
+//global varriables
 
 
 var cards = [
@@ -29,6 +30,12 @@ var cards = [
 
 var cardsInPlay = []
 
+//when the varriable i is smaller than the lengths of elements in cards add one to i,
+//
+
+
+
+// when cardsinplay is equal in value , you log  " you found a match", otherwise log " sorry try again"
 function checkForMatch (){
 
     if ( cardsInPlay[0] === cardsInPlay[1]) {
@@ -38,14 +45,20 @@ function checkForMatch (){
       }
 };
 
-function flipCard (cardId){
+function flipCard () {
+    var cardId = this.getAttribute('card-Id');
+    
 
     console.log("User flipped" + cards[cardId].rank)
     cardsInPlay.push (cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
     console.log (cards[cardId].suit);
     console.log (cards[cardId].cardImage);
- 
+ //when there are 2 cards in play and they match faces , alert the user that they found a match
+ //otherwise alert the user to try again
 checkForMatch ();
+}
+   
 if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1] ) {
     
 alert("You Found a match!");
@@ -54,6 +67,17 @@ else  {
     (cardsInPlay.length === 2 && cardsInPlay[0] !== cardsInPlay[1] )
 alert("sorry,try again ! ");
 }
-};
-flipCard (0);
-flipCard (1);
+
+
+        function createBoard() { 
+            const cardTable = document.getElementById('game-board');
+    for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+      cardElement.setAttribute('src', 'images/back.png');
+      cardElement.setAttribute('data-id', i);
+      cardElement.addEventListener('click', flipCard);
+      cardTable.appendChild(cardElement);
+        }
+  };
+  
+  createBoard();
